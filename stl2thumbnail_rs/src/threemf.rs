1189dbeeb42691da4cd1b0a55800212ef2de5f83
Paul <krepa098@gmail.com>
2025-03-07 21:47:33 +0100

    3mf: support bambu flavor resolves #5

--- stl2thumbnail_rs/src/threemf.rs
+++ stl2thumbnail_rs/src/threemf.rs
@@ -1,9 +1,8 @@
+use anyhow::{bail, Result};
 use std::{
     io::{Read, Seek},
     path::Path,
 };
-
-use anyhow::Result;
 
 use crate::picture::Picture;
 
@@ -18,23 +17,42 @@ where
 {
     let mut archive = zip::ZipArchive::new(r)?;
 
-    let mut thumbnail = archive.by_name("Metadata/thumbnail.png")?;
-    let mut buffer = vec![];
-    thumbnail.read_to_end(&mut buffer)?;
-    let image = image::load_from_memory(&buffer)?;
+    // depending on the slicer, the thumbnail gets stored in different locations
+    //
+    // Prusa, Cura: 'Metadata/thumbnail.png'
+    // Bambu, Orca: 'Metadata/plate_<plate-number>.png'
+    //
+    // Prusa slicer generates a single thumbnail for all build plates
+    // Bambu slicer generates one thumbnail per build plate (we show the thumbnail of the first build plate)
 
-    Ok(Picture::from_img_buffer(image.to_rgba8()))
+    for filename in ["Metadata/thumbnail.png", "Metadata/plate_1.png"] {
+        if let Ok(mut thumbnail) = archive.by_name(filename) {
+            let mut buffer = vec![];
+            thumbnail.read_to_end(&mut buffer)?;
+            let image = image::load_from_memory(&buffer)?;
+
+            return Ok(Picture::from_img_buffer(image.to_rgba8()));
+        }
+    }
+
+    bail!("Cannot find thumbnail in 3mf")
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    static TEST_FILE: &[u8] = include_bytes!("../test_models/prusa_test.3mf");
+    static PRUSA_TEST_FILE: &[u8] = include_bytes!("../test_models/prusa_test.3mf");
+    static BAMBU_TEST_FILE: &[u8] = include_bytes!("../test_models/bambu_test.3mf");
 
     #[test]
     pub fn test_extract_preview() {
-        let cursor = std::io::Cursor::new(TEST_FILE);
+        let cursor = std::io::Cursor::new(PRUSA_TEST_FILE);
+        let preview = extract_preview(cursor);
+
+        assert!(preview.is_ok());
+
+        let cursor = std::io::Cursor::new(BAMBU_TEST_FILE);
         let preview = extract_preview(cursor);
 
         assert!(preview.is_ok());
